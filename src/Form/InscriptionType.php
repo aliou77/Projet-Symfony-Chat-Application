57fb15ca1958975b4fbea371d373901ec6719f33
aliou <aliou@gmail.com>
2023-05-10 02:25:03 +0200

    feat(inscription): mise en place de la page d'inscription

---
+++ src/Form/InscriptionType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fname', TextType::class, [
+                'label' => false,
+                'attr' =>[
+                    'placeholder' => "First Name",
+                    'autofocus' => true
+                ]
+            ])
+            ->add('lname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Last Name"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Email",
+                    'value' => false
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Phone"
+                ]
+            ])
+            ->add('password', RepeatedType::class, [ // permet de faire duplicaiton du champs pour comfirmer le password
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['id' => 'password']],
+                'required' => true,
+                'first_options'  => ['label' => false, 'attr' => ['placeholder' => 'Password']],
+                'second_options' => ['label' => false, 'attr' => ['placeholder' => 'Comfirm Password']],
+            ])
+            ->add('description', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Describe your self in few words..."
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
